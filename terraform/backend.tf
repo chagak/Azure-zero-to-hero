2908072383d7436d6b83975c3c016289ae5904d2
chagak <kchaggak@gmail.com>
2025-01-26 12:46:29 -0600

    secrete

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,6 +6,6 @@ terraform {
     storage_account_name = "chaganotestorage"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "backend-tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    #access_key           = "M2B3CK72/FhCNxN1ecpxLMODLFuUlC+qWAkls+PS4Ga6qIb4U73Lad9+v96tOLGz14Xr2P12wcmR+ASt+3aU+A=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    access_key           = "...."  # Can also be set via `ARM_ACCESS_KEY` environment variable.
   }
 }
